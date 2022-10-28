27c128bffb949715ab778e64f70bb743387c4b4d
0x78logs <73669345+0x78logs@users.noreply.github.com>
2022-10-28 22:03:00 +0100

    Make demo rely directly on index.js instead of min.js

---
+++ demo/app.js
@@ -0,0 +1,26 @@
+import {uhu} from "../index.js";
+
+const on = name => cb => element =>
+  element.addEventListener (name, cb);
+
+const select = target =>
+  document.querySelectorAll (target)[0];
+
+const T = x => f => f (x);
+const $ = T;
+
+// slider
+const $size = select (".sizerange input");
+const $height = select (".heightrange input");
+const $update_btn = select (".update");
+
+const update_layout = () => uhu ($size.value, $height.value);
+
+// on events
+$ ($size) (on ("input") (update_layout));
+$ ($height) (on ("input") (update_layout));
+$ ($update_btn) (on ("click") (update_layout));
+
+// on page load
+uhu (1, 3);
+
